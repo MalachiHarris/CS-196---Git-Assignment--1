16e7d1832147db11125f80d2aa78dae885f97f9a
MalachiHarris <156547558+MalachiHarris@users.noreply.github.com>
2024-01-18 13:12:19 -0800

    Put in code for assignment #2

--- GHPH/TestPage.js
+++ GHPH/TestPage.js
@@ -1,21 +1,45 @@
 // Malachi Harris
-// 1-13-24
+// 1-18-24
 
+
+
+
+
+
+
+
+
+//for assignment 2:loops and conditionals
+var Value = 42;
+
+for(i=0;i<5;i++)
+{
+    if (Value % 2 == 1)
+    {
+        console.log("Value is odd and currently equals " + Value + ".");
+    }
+
+    Value++;
+}
+
+
+
+// for assignment 1: console.log(), prompt(), and Basic Variables 
 
 
 //create var, => do some operation on its value
-var armadilloCount = 0;
-armadilloCount = armadilloCount + 1;
+// var armadilloCount = 0;
+// armadilloCount = armadilloCount + 1;
 
 
 
 
-var userEntry = prompt("How many armadillos have you seen?:");
+// var userEntry = prompt("How many armadillos have you seen?:");
 
 
-armadilloCount = armadilloCount + parseInt(userEntry, 10);
+// armadilloCount = armadilloCount + parseInt(userEntry, 10);
 
-alert("Together we have seen " + armadilloCount + " armadillos");
+// alert("Together we have seen " + armadilloCount + " armadillos");
 
-console.log("armadilloCount", armadilloCount);
-console.log("userEntry", userEntry);
+// console.log("armadilloCount", armadilloCount);
+// console.log("userEntry", userEntry);
