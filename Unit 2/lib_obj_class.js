aff3d681341cd151fe9d093862cba30617f4e760
MalachiAHarris <biz.mh@pm.me>
2024-02-24 14:10:27 -0600

    Added lib_obj_class.js, objects.js & objects.html

---
+++ Unit 2/lib_obj_class.js
@@ -0,0 +1,16 @@
+// Create a class
+class Pets {
+    // Constructor method that sets property values to parameters
+    constructor(name, kind) { 
+        this.name = name; 
+        this.kind = kind;
+    }
+
+    // Method that logs property names to the console
+    printName() {
+        console.log("Name: " + this.name +"\nKind: " + this.kind); 
+
+    }
+}
+
+
---
+++ Unit 2/objects.js
@@ -0,0 +1,8 @@
+// Create two objects from library
+const person1 = new Pets("Squeaky","Bird"); 
+const person2 = new Pets("Zues", "Dog"); 
+
+
+//call method that logs property names to the console
+person1.printName(); 
+person2.printName();
